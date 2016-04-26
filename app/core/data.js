9c060f39f0b19b1e6cc9693d995948449909d753
Christian Vargas <cvargas820@gmail.com>
2016-04-26 10:33:08 -0400

    Initial commit

---
+++ app/core/data.js
@@ -0,0 +1,109 @@
+module.exports = {
+  myNews: [{
+    co: 'Apple Inc.',
+    h: 'ClimateCare Reports Impressive Figures',
+    s: 'CSR Wire',
+    c: 'Apr 09, 2016',
+    d: 'Apr 11, 2016',
+    i: 13,
+    r: 41,
+    b: 'Climate and sustainable development expert ClimateCare today announced new impact figures that show it has improved life for 16.5 million people and at the same time...',
+  },{
+    co: 'Yelp',
+    h: 'U.S. Chamber Foundation Seeks Nominations to Recognize Top Corporate Citizens',
+    s: 'CSR Wire',
+    c: 'Apr 06, 2016',
+    d: 'Apr 10, 2016',
+    i: 8,
+    r: 15,
+    b: 'Climate and sustainable development expert ClimateCare today announced new impact figures that show it has improved life for 16.5 million people and at the same time...',
+  },{
+    co: 'United Airlines',
+    h: 'Russian Military Registers Seven Ceasefire Violations in Syria',
+    s: 'Bloomberg',
+    i: 9,
+    r: 33,
+    c: 'Apr 02, 2016',
+    d: 'Apr 03, 2016',
+    b: '',
+  },{
+    co: 'Apple Inc.',
+    h: 'Gulf markets stabilise, Egypt rebounds',
+    s: 'Bloomberg',
+    i: 9,
+    r: 33,
+    c: 'Apr 02, 2016',
+    d: 'Apr 03, 2016',
+    b: '',
+  }],
+
+  company: {
+    name: 'Uber Technologies Inc.',
+    sector: 'Technology',
+    description: '<strong>Uber Technologies Inc.</strong> is an American multinational online transportation network company headquartered in San Francisco, California. It develops, markets and operates the Uber mobile app, which allows consumers with smartphones to submit a trip request which is then routed to Uber drivers who use their own cars. As of April 12, 2016, the service was available in over 60 countries and 404 cities worldwide. Since Uber\'s launch, several other companies have copied its business model, a trend that has come to be referred to as "Uberification".<br /><br />Uber was founded as "UberCab" by Travis Kalanick and Garrett Camp in 2009 and the app was released the following June. Beginning in 2012, Uber expanded internationally. In 2014, it experimented with carpooling features and made other updates. Klout ranked the San Francisco-based company as the 48th-most powerful company in America in 2014. By late-2015, Uber was estimated to be worth $62.5 billion.',
+    employees: '1,200',
+    address: '1455 Market St<br />San Francisco, CA 94103',
+    phone: '(415) 801-4068',
+    url: 'http://www.uber.com',
+    headquarter: 'United States',
+    asset: 'United States',
+    revenue: 'Canada',
+    rrr: 62.3,
+    investor: 47.5,
+    corporate: 56.1
+  },
+
+  saved: [
+    "Lyft",
+    "Curb",
+    "Taxi Magic",
+    "Didi Kuaidi",
+    "Grab",
+    "Ola"
+  ],
+
+  watchlist: [{
+    n: "Key Competitors",
+    co: 6,
+    rrr: 71,
+    peak: 79,
+    avg: 66,
+    c: "04/03/2016",
+    u: "04/25/2016",
+  },{
+    n: "Technology Partners",
+    co: 31,
+    rrr: 52,
+    peak: 68,
+    avg: 61,
+    c: "04/07/2016",
+    u: "04/18/2016",
+  }],
+
+  search: [{
+    name: "American Apparel",
+    sector: "Retailer",
+    rrr: 77.2,
+  },{
+    name: "Apple Inc.",
+    sector: "Technology",
+    rrr: 62.4,
+  },{
+    name: "Applied Genetic Technologies",
+    sector: "Technology",
+    rrr: 51.9,
+  },{
+    name: "Neuberger Berman Holdings LLC",
+    sector: "Diversified Financials",
+    rrr: 17.2,
+  },{
+    name: "Huber & Suhner AG",
+    sector: "Electrical Equipment",
+    rrr: 72.4,
+  },{
+    name: "Uber Technologies Inc.",
+    sector: "Technology",
+    rrr: 62.3,
+  }],
+
+};
